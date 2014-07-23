6b5099f0ebd9051f7634406a8252b725b88cf873
devinacker <d@revenant1.net>
2014-07-23 01:12:51 -0400

    replace build script with makefile

---
+++ Makefile
@@ -0,0 +1,2 @@
+CXXFLAGS += -std=gnu++0x -s -I.
+xkas: xkas.cpp libxkas/libxkas.cpp
--- cc.bat
+++
@@ -1,5 +0,0 @@
-mingw32-g++ -std=gnu++0x -I. -c xkas.cpp
-mingw32-g++ -std=gnu++0x -I. -c libxkas/libxkas.cpp
-mingw32-g++ -o xkas xkas.o libxkas.o
-strip -s xkas.exe
-@del *.o
